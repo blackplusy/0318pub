34ded54896711f87962cd30da8f7009f09bd5261
blackplusy <happyming07@sina.com>
2019-03-28 15:47:13 +0800

    Add files via upload

---
+++ 例子-0328-05.逻辑运算符.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+# 假 and  真==假
+print(1>2 and 2==2)
+
+#假 or 真==真
+print(2>1 or 3==8)
+
+#非 假==真
+print(not(3>5))
---
+++ 例子-0328-06.成员运算符.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+a='heygor memeda!!!!!'
+#取出a变量中所有元素
+for i in a:
+    print(i)
+#取出列表l中所有元素
+l=[1,2,3,4]
+for i in l:
+    print(i)
+b='hello memeda!!'
+if 'l' in b:
+    print('l is here!!')
