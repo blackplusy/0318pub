084d4b28f5cb44ec3af06573e2cab397cbbe3fb5
blackplusy <happyming07@sina.com>
2019-03-28 16:34:03 +0800

    Add files via upload

---
+++ 例子-0328-08.双分支.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+name=input('请输入您喜欢的主播：')
+if name=='55开':
+    print('我没开挂')
+else:
+    print('刷波飞机，办张卡')
---
+++ 例子-0328-09.多分支.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+name=input('请输入您喜欢的主播：')
+if name=='大司马':
+    print('三角形打野')
+elif name=='张大仙':
+    print('不慌，吃口药！')
+elif name=='发儿':
+    print('个子不高')
+else:
+    print('竟然不如黑哥')
