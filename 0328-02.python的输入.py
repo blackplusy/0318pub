a4b90046beafdc29569f5529ed63cd8650fb9712
blackplusy <happyming07@sina.com>
2019-03-28 10:52:16 +0800

    Add files via upload

---
+++ 0328-02.python的输入.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+#input函数默认没有提示信息，可以把提示信息写入input的括号中
+#name=input()
+#print('您输入的名字是： %s'%name)
+name=input('请输入您的法号！！！ ')
+print(type(name))
+print('英雄的法号是%s'% name)
+#type函数可以判断变量的数据类型
+#补充：数据类型转换
+#int 转换为数字类型
+#str 转换为字符类型
+age=input('请输入您的年龄：')
+print('您的年龄是%d'%int(age))
